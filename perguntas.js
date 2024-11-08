755c6d71f24bd6e917b8215dd11a4ca6d6877c46
lucaspomba <141828588+lucaspomba@users.noreply.github.com>
2024-11-08 08:45:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Um espaço para armazenar dados.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre uma função e um método?',
+    'Função é independente; método pertence a um objeto.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um erro de sintaxe?',
+    'Erro de escrita que impede execução.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que servem os operadores condicionais?',
+    'Comparar valores e tomar decisões.'
+)
